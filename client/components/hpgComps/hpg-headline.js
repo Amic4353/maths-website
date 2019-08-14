59a46648aae3047a15a1aea7e73234183a5c18b5
amaris chang <amaris.a.chang@gmail.com>
2019-08-14 16:53:43 -0400

    [feat]React - adds scss for hpg-headline

--- client/components/hpgComps/hpg-headline.js
+++ client/components/hpgComps/hpg-headline.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function Headline() {
   return (
-    <div>
-      <h1>Maths Grinds Junior Cert | Maths Grinds Leaving Cert</h1>
-      <div>
+    <div id="headline">
+      <div id="headline-text">
+        <h1>Maths Grinds Junior Cert | Maths Grinds Leaving Cert</h1>
+      </div>
+      <div id="headline-box">
         <p>
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum. lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum.
+          Looking for Project Maths Grinds in Junior and Leaving Certificate, I
+          offer a unique learning experience for anyone wanting to learn the new
+          Project Maths syllabus.
         </p>
       </div>
     </div>
