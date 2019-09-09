41f980cc44bfb2a706ac6612c42d68ffe7774a87
amaris chang <amaris.a.chang@gmail.com>
2019-09-09 16:04:26 -0400

    adds about page

--- client/components/about.tsx
+++ client/components/about.tsx
@@ -1,28 +1,57 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {withStyles} from '@material-ui/core/styles'
+
+import Button from '@material-ui/core/Button'
+
+const ColorButton = withStyles(theme => ({
+  root: {
+    fontFamily: `'Gentium Basic', serif`,
+    color: '#f0f0f0',
+    backgroundColor: '#009388',
+    '&:hover': {
+      backgroundColor: '#3c4043'
+    }
+  }
+}))(Button)
 
 export default function About() {
-  const images = []
+  const images = [
+    'https://i.imgur.com/MLSvlVR.jpg',
+    'https://i.imgur.com/G0POMkF.jpg',
+    'https://i.imgur.com/Sq2PPwr.jpg',
+    'https://i.imgur.com/nNexUFk.jpg',
+    'https://i.imgur.com/eboDPRA.jpg',
+    'https://i.imgur.com/hZwfPan.jpg',
+    'https://i.imgur.com/3aKQgtG.jpg'
+  ]
   return (
     <div id="about-container">
-      <h1>{`About & Publications`}</h1>
-      <div id="about-content">
-        <img src="" />
-        <p>
-          Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-          ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-          ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-          ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-          ipsumLorem Lorem ipsumLorem ipsumLorem ipsum
-        </p>
-      </div>
-      <h2>Publications</h2>
-      <div id="about-img-grid">
-        {images.map((img, i) => (
-          <div id={`aboutImg${i}`} key={i}>
-            <img src={img} />
-          </div>
-        ))}
+      <div id="about-column">
+        <h1>{`About & Publications`}</h1>
+        <div id="about-content">
+          <img src="https://i.imgur.com/MbqW7Bb.jpg" />
+          <p>
+            Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            ipsumLorem Lorem ipsumLorem ipsumLorem ipsum ipsumLorem ipsumLorem
+            ipsumLorem Lorem ipsumLorem ipsumLorem ipsum
+          </p>
+        </div>
+        <h2>Publications</h2>
+        <div id="about-img-grid">
+          {images.map((img, i) => (
+            <div id={`aboutImg${i}`} key={i}>
+              <img src={img} />
+            </div>
+          ))}
+
+          <ColorButton className="colorBtn" variant="contained">
+            Check Out Courses
+          </ColorButton>
+        </div>
       </div>
     </div>
   )
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,46 +3,17 @@ import AppBar from '@material-ui/core/AppBar'
 import {NavLink} from 'react-router-dom'
 import history from '../history'
 
-export default function NavBar(props) {
-  if (props.currPage === '/about') {
-    return (
-      <div>
-        <AppBar id="navBar" position="static">
-          <NavLink to="">
-            <i className="material-icons">school</i>
-          </NavLink>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="">Course Schedule</NavLink>
-          <NavLink to="">Contact Info</NavLink>
-        </AppBar>
-      </div>
-    )
-  }
-  if (props.currPage === '/courses') {
-    return (
-      <div>
-        <AppBar id="navBar" position="static">
-          <NavLink to="">
-            <i className="material-icons">school</i>
-          </NavLink>
-          <NavLink to="">Home</NavLink>
-          <NavLink to="/about">About & Publications</NavLink>
-          <NavLink to="">Contact Info</NavLink>
-        </AppBar>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <AppBar id="navBar" position="static">
-          <NavLink to="">
-            <i className="material-icons">school</i>
-          </NavLink>
-          <NavLink to="/about">About & Publications</NavLink>
-          <NavLink to="">Course Schedule</NavLink>
-          <NavLink to="">Contact Info</NavLink>
-        </AppBar>
-      </div>
-    )
-  }
+export default function NavBar() {
+  return (
+    <div>
+      <AppBar id="navBar" position="static">
+        <NavLink to="">
+          <i className="material-icons">school</i>
+        </NavLink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About & Publications</NavLink>
+        <NavLink to="">Course Schedule</NavLink>
+      </AppBar>
+    </div>
+  )
 }
